345a58924b25e0a62d3447dcd8495448d23c8daa
ViniciusTG <User@DESKTOP-27FEHD8>
2022-05-10 20:21:44 -0300

    Adicionar arquivos de projeto.

---
+++ ControladorFaturamento/Controllers/ControladorFaturamentoController.cs
@@ -0,0 +1,29 @@
+﻿using ControladorFaturamento.Dominio;
+using ControladorFaturamento.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControladorFaturamento.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ControladorFaturamentoController : Controller
+    {
+        public IActionResult Processar(FiltroFaturamento filtro)
+        {
+            try
+            {
+                new ControladorFaturamentoService().ProcessarFila(filtro);
+
+                return Ok(filtro);
+            }
+            catch (Exception ex)
+            {
+                return new StatusCodeResult(500);
+            }
+        }
+    }
+}
---
+++ ControladorFaturamento/Services/ControladorFaturamentoService.cs
@@ -0,0 +1,39 @@
+﻿using ControladorFaturamento.Dominio;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ControladorFaturamento.Services
+{
+    public class ControladorFaturamentoService
+    {
+        public void ProcessarFila(FiltroFaturamento filtro)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "filaFaturador",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                filtro.Id = new Random().Next(777777, 999999);
+                filtro.CodigoCliente = new Random().Next(4000, 9999);
+
+                string mensagem = JsonSerializer.Serialize(filtro);
+                var corpoMensagem = Encoding.UTF8.GetBytes(mensagem);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "filaFaturador",
+                                     basicProperties: null,
+                                     body: corpoMensagem);                
+            }            
+        }
+    }
+}
